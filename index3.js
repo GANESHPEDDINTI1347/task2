77b02748e2c553d73ecf1e8ada760217f9dc4c1d
GANESHPEDDINTI1347 <ganeshpeddinti40@gmail.com>
2025-06-26 18:14:33 +0530

    updated code

--- index3.js
+++ index3.js
@@ -1,7 +1,6 @@
 const display = document.getElementById("display");
 const buttons = document.querySelectorAll(".btn");
 let current = "";
-
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         const value = button.dataset.value;
